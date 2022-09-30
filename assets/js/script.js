55b10b63499a2101b9769f973d197e24d3507017
AJMANZ <113150864+AJManzione@users.noreply.github.com>
2022-09-29 21:06:08 -0600

    commenting

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@ var specialCharacters = "!@#$%^&*()_+~`|}{[]\:;?><,./-=";
 var selection = [];
 
 
-//global variables set to no unless user specifies otherwise
+//Global variables set to NONE unless user specifies otherwise
 var passwordPrint = [];
 var passwordGenerated = [];
 var passwordSize = 0;
@@ -18,22 +18,23 @@ var numbersIncluded = false;
 var specialCharactersIncluded = false;
 
 
-// this function gets called at the very end of the code and print's the appropriate 
+// And Lastly,
+// This function gets called at the very end of the code and print's the appropriate password, followed by a refresh of the page!!!
 function printPassword() {
   var refresh = alert ("your password is " + passwordPrint + "\n \n Click ok to refresh");
   if (refresh == true) {
   }
-  document.location.reload(true);
+  document.location.reload(true); //refresh the page after user presses okay, this gets rid of all the data after they press okay.. >:)
 }
 
 
-
+// connects the "generate password" button that is tied to html to the first prompt for the user/
 document.querySelector('#generate').addEventListener('click', inputPrompt); // press a button on the webpage
 
 
 
 
-// prompts that change global variables to True dependant on user behavior. 
+// Prompt one 'User must enter a value between 8 - 128, otherwise repromtped.
 
 function inputPrompt() {
 
@@ -47,39 +48,49 @@ function inputPrompt() {
       alert("You entered a number greater than 128 \nYou must enter a number between 8 - 128");
     } else {
       alert("You did not enter any number");
-      inputPrompt();
+      inputPrompt();   
     }
 }
 
+
+// Prompt two ' Including lowercases to the password, if true the global variable gets assigned
+
 function promptOne() {
 
-  var userLowerCase = confirm ("Would you like to include lowercase in your password? \n(example: a, b, c, d, etc...)"); 
+    var userLowerCase = confirm ("Would you like to include lowercase in your password? \n(example: a, b, c, d, etc...)"); 
 
- if (userLowerCase == true) {
-  lowerCaseIncluded = userLowerCase; //asigns the global variable to the user input
-  promptTwo();
-  } else promptTwo();
+    if (userLowerCase == true) {
+    lowerCaseIncluded = userLowerCase; //asigns the global variable to the user input
+    promptTwo();
+    } else promptTwo();
 }
+
+
+// Prompt two  Including Uppercases to the password, if true the global variable gets assigned
 
 function promptTwo() {
 
-  var userUpperCase = confirm ("Would you like to include UPPERCASE in your password? \n(example: A, B, C, D, etc...)"); 
+    var userUpperCase = confirm ("Would you like to include UPPERCASE in your password? \n(example: A, B, C, D, etc...)"); 
 
-  if (userUpperCase == true) {
-  upperCaseIncluded = userUpperCase; // assigns the global variable to the user input
-  promptThree();
-  } else promptThree();
+    if (userUpperCase == true) {
+    upperCaseIncluded = userUpperCase; // assigns the global variable to the user input
+    promptThree();
+    } else promptThree();
 }  
+
+// Prompt three  Including Numbers to the password, if true the global variable gets assigned
 
 function promptThree() {
 
-  var userNumbers = confirm ("Would you like to include NUMBERS in your password? \n(example: 1, 2, 3, 4, etc...)"); 
+    var userNumbers = confirm ("Would you like to include NUMBERS in your password? \n(example: 1, 2, 3, 4, etc...)"); 
 
- if (userNumbers == true) {
-  numbersIncluded = userNumbers; //asigns the global variable to the user input
-  promptFour();
-  } else promptFour();
+    if (userNumbers == true) {
+    numbersIncluded = userNumbers; //asigns the global variable to the user input
+    promptFour();
+    } else promptFour();
 }
+
+// Prompt four Including special  to the password, if true the global variable gets assigned
 
 function promptFour() {
 
@@ -89,18 +100,13 @@ function promptFour() {
     specialCharactersIncluded = userSpecialCharacters;
     createRandomPassword();
     } else { alert("Your password is being generated");
-    createRandomPassword();
+    createRandomPassword(); // function gets called that checks conditional statements below 
     }
-  
 }
  
 
 
-
-//randomly produces oass
-
-
-
+/* This function contains conditionals that assign the uppercase, lowercase, numbers, or special characters to an array that was classified as empty depending on what the user selected in prompts 1 - 4 */
 
 function createRandomPassword() {
 
@@ -120,13 +126,16 @@ function createRandomPassword() {
    if (specialCharactersIncluded === true) {
     selection = selection + specialCharacters;
    }
- 
+
+/* still within the function but after the conditionals, the for loop adds a random character(num, upper, lower, symbol) to the variable called generatedPassword by using the .push method.. essentionally the variable "selection(containing user criteria)"" is being called at an array position by a random num "randomNum" the elements of the array gets randomly called and pushed to the variable "passwordGenerated" one character at a time and includes commas. */
+
+  
    for (var i=0; i < passwordSize; i++) {
 		var randomNum = Math.floor(Math.random() * selection.length);
     passwordGenerated.push(selection[randomNum]);
-    passwordPrint = passwordGenerated.join("");
-    }
-
+    passwordPrint = passwordGenerated.join("");  // since we dont want commas to be included, we use the .join method to sperate the  //
+    }                                            // elements of the array and convert them into one string and get rid of the commas  //
+                                                 // by including quotations inside the .join("") parameters.
    return printPassword();
-}
+}                           // the code then jumps back to the top //
 
